9e2c1dea2597a8ece9f01c51d70e3aee2990712e
Patrick Byrne <code@patrickbyrne.net>
2009-06-28 07:53:09 -0500

    added rdoc to the source, with examples

--- lib/valid-date.rb
+++ lib/valid-date.rb
@@ -2,6 +2,14 @@
 require 'activesupport'
 
 class String
+  # 
+  # Determines whether the string contains a valid date for use with the Rails String#to_date method.
+  # 
+  #   "2008-01-01".valid_date? #=> true
+  #   "20080101".valid_date?   #=> true
+  #   "0000-00-00".valid_date? #=> false
+  #   "".valid_date?           #=> nil
+  # 
   def valid_date?
     unless self == ''
       begin
